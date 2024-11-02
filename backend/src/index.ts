4bd6a127292088cae1151a50aa0fe29b9943d125
brazillierjo <j.brazillier@gmail.com>
2024-11-02 21:36:49 +0100

    feat: setup prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,23 @@
-import { cookie } from "@elysiajs/cookie";
-import { jwt } from "@elysiajs/jwt";
+import {cookie} from "@elysiajs/cookie";
+import {jwt} from "@elysiajs/jwt";
 import Elysia from "elysia";
+import {auth} from "./modules/auth";
 
 const app = new Elysia()
-  // Route Hello World en dehors du groupe /api
-  .get("/", () => "Hello World!")
-  // ou si vous préférez un objet JSON
-  .group("/api", (app) =>
-    app
-      .use(
-        jwt({
-          name: "jwt",
-          secret: Bun.env.JWT_SECRET!,
-        })
-      )
-      .use(cookie())
-  )
-  .listen(3001);
+    .get("/", () => "Hello World!")
+    .group("/api", (app) =>
+        app
+            .use(
+                jwt({
+                    name: "jwt",
+                    secret: Bun.env.JWT_SECRET!,
+                })
+            )
+            .use(cookie())
+            .use(auth)
+    )
+    .listen(3001);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
