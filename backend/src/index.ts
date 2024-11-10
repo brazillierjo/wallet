77d98451a220d8438fce07f143608b9e39859416
brazillierjo <j.brazillier@gmail.com>
2024-11-10 19:50:06 +0100

    feat: clean console.logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,10 +7,6 @@ dotenv.config({
     path: path.resolve(__dirname, "../../.env"),
 });
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-console.log(JWT_SECRET);
-
 const app = new Elysia({prefix: "/api"}).get("/", () => "Hello World!").use(authRoutes).listen(3001);
 
 console.log(
