033d5171fab4ffbdccc323aa145aa8724133ff4c
brazillierjo <j.brazillier@gmail.com>
2025-04-07 22:16:43 +0200

    refactor: update layout to include max width for improved responsiveness

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({ children, params }: RootLayoutProps) 
 
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={cn(appFont.className)}>
+      <body className={cn(appFont.className, "mx-auto max-w-screen-2xl")}>
         <NextIntlClientProvider>
           <Providers>{children}</Providers>
         </NextIntlClientProvider>
--- todo.md
+++ todo.md
@@ -1,7 +1,5 @@
 // create admin dashboard with refine to see database
-// handle layout size max on wide screen
 // add user profile picture on header protected routes
-// add theme toggle on free routes
 // change position language selector
 
 // remove zoom on mobile input
