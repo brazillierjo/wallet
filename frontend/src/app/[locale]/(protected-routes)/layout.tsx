ceeafb90a5cc0c3bbdf448197597c84f115a8434
brazillierjo <j.brazillier@gmail.com>
2025-04-07 20:55:25 +0200

    feat: implement user authentication check and loading state in AuthLayout

--- frontend/src/app/[locale]/(protected-routes)/layout.tsx
+++ frontend/src/app/[locale]/(protected-routes)/layout.tsx
@@ -1,13 +1,30 @@
-import { FC, ReactNode } from "react";
+"use client";
+
+import { FC, ReactNode, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import Footer from "@/components/Footer/Footer";
+import Loading from "@/components/Loading";
 import ProtectedSidebar from "@/components/ProtectedSidebar";
+import { useGetUser } from "@/hooks/mutations/user/useGetUser";
+import { AppRoutes } from "@/router/app_routes";
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const { data: userResponse, isLoading } = useGetUser();
+  const user = userResponse?.data?.user;
+
+  useEffect(() => {
+    if (!isLoading && !user) router.push(AppRoutes.AUTH);
+  }, [user, isLoading, router]);
+
+  if (isLoading) return <Loading />;
+  if (!user) return null;
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <ProtectedSidebar>
