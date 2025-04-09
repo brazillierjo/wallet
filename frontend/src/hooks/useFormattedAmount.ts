f1ea7edd487de1dbb1582d5e538e83c90118a886
brazillierjo <j.brazillier@gmail.com>
2025-04-09 20:35:20 +0200

    fix: adjust formatting logic to handle whole numbers correctly in useFormattedAmount hook

--- frontend/src/hooks/useFormattedAmount.ts
+++ frontend/src/hooks/useFormattedAmount.ts
@@ -10,11 +10,14 @@ interface UseFormattedAmountProps {
 export function useFormattedAmount({ locale, amount }: UseFormattedAmountProps): string {
   const currency = getCurrencyFromLocale(locale);
 
+  // Check if the amount is a whole number
+  const isWholeNumber = Math.floor(amount) === amount;
+
   const formattedAmount = new Intl.NumberFormat(locale, {
     style: "currency",
     currency,
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2,
+    maximumFractionDigits: isWholeNumber ? 0 : 2,
+    minimumFractionDigits: isWholeNumber ? 0 : 2,
   }).format(amount);
 
   return formattedAmount;
